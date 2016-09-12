ece00973e2eac67d164c2a2737a75a60dad7f04f
Jay Gurnani <jay.gurnani@gmail.com>
2016-09-12 15:44:59 +1000

    Lots of changes. No commit in a while.

--- VelocityTime.R
+++ VelocityTime.R
@@ -3,35 +3,43 @@ setwd("/Users/mac/Desktop/Programming/COMP9596//DroneData")
 require(splines) #thx @Chase for the notice
 
 # Read the data
-data <- read.csv(file="ZigZag.csv", header=TRUE, sep=",")
+data <- read.csv(file="circlezigzag.csv", header=TRUE, sep=",")
+data2 <- read.csv(file="45mscircle.csv", header=TRUE, sep=",")
 
 # Convert date column to Date object
 data$Date <- as.POSIXct(data$Date)
 
 # Extract the data from column name to plot
 data.toPlot <- data[data$Battery > 22,]
+data2.toPlot<- data2[data2$Battery > 22,]
 
 # Calculate the difference of each time point to the first (in seconds)
 data.toPlot$TimeDiff <- data.toPlot$Date - data.toPlot$Date[1]
+data2.toPlot$TimeDiff <- data.toPlot$Date - data.toPlot$Date[1]
 
 # Since TimeDiff is a time/date object, convert it to a numeric and store this as an extra column in data frame
 data.toPlot$TimeDiffValue <- as.numeric(data.toPlot$TimeDiff)
+data.toPlotFinala <- data[data.toPlot$TimeDiffValue < 6]
 
 # Multiple current and voltage to get Power
 data.toPlot$Power <- as.double(-(data.toPlot$Current/1000) * data.toPlot$Voltage/1000)
+data2.toPlot$Power <- as.double(-(data.toPlot$Current/1000) * data.toPlot$Voltage/1000)
 
 #Get Mangnitude of Velocity
 data.toPlot$Velocity <- sqrt(data.toPlot$VelocityX^2 + data.toPlot$VelocityY^2 + data.toPlot$VelocityZ^2)
-#data.toPlotFinal <- data.toPlot[with(data.toPlot, order(VelocityX)), ]
+data2.toPlot$Velocity <- sqrt(data.toPlot$VelocityX^2 + data.toPlot$VelocityY^2 + data.toPlot$VelocityZ^2)
+#data.toPlotFinal <- data.toPlot[data.toPlot$TimeDiffValue > 9 & data.toPlot$TimeDiffValue < 17, ]
 
 #dY <- diff(data.toPlot$Velocity)/diff(data.toPlot$TimeDiffValue)  # the derivative of your function
 #dX <- rowMeans(embed(data.toPlot$TimeDiffValue,2)) # centers the X values for plotting
-#par(mar=c(5,5,2,2))
-#powerTest <- data.toPlot$Power[1:923]
-#ppi <- 200
-#png(file=paste("ZigZagVelocity", ".png", sep=""), height=7*ppi, width=9*ppi, res=ppi)
 
-plot(y=data.toPlot$Wifi6, x=data.toPlot$TimeDiffValue,type="l",col="black", lwd="1", ylab=expression(~Accleration ~(m/s^{2})), xlab="Time (seconds)")
+#powerTest <- data.toPlot$Power[1:141]
+#ppi <- 200
+#png(file=paste("circlezigzag", ".png", sep=""), height=7*ppi, width=9*ppi, res=ppi)
+
+#write.csv(data.toPlotFinal, file="FirstRev.csv")
+plot(y=data.toPlot$DroneHeading, x=data.toPlot$TimeDiffValue,type="l",col="black", lwd="1", ylab="Velocity (m/s)", xlab="Power (Watts)")
+#points(y=data2.toPlot$Power, x=data.toPlot$TimeDiffValue)
 #abline(lm(data.toPlot$Power ~ data.toPlot$Velocity), col="red")
 #legend("topright", legend=c("Mean Watts = 154.5845"), pch=1, lty=1, lwd=3)
 
