629957d2d7edf5ac65b3317cfc22068f4af1ced9
Jay Gurnani <jay.gurnani@gmail.com>
2016-10-21 17:25:33 +1100

    Azade requested graphs complete

--- VelocityTimeFirstRun.R
+++ VelocityTimeFirstRun.R
@@ -4,7 +4,7 @@ require(splines) #thx @Chase for the notice
 library(geosphere)
 
 # Read the data
-data <- read.csv(file="5msAngles.csv", header=TRUE, sep=",")
+data <- read.csv(file="Azade5ms1sec.csv", header=TRUE, sep=",")
 
 # Convert date column to Date object
 data$Date <- as.POSIXct(data$Date)
@@ -28,24 +28,24 @@ data.toPlot$Mean <- mean(data.toPlot$Power)
 
 
 ppi <- 200
-#png(file=paste("VelocityTurningIntervals5ms", ".png", sep=""), height=7*ppi, width=9*ppi, res=ppi)
+png(file=paste("5ms1secInterval", ".png", sep=""), height=7*ppi, width=9*ppi, res=ppi)
 
 #write.csv(data.toPlotFinal, file="FirstRev.csv")
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+#par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
 plot(y=data.toPlot$Velocity, x=data.toPlot$TimeDiffValue,type="l",col="black", lwd="1", ylab="Power (in Watts)", xlab="Time in Seconds",
-     main="Effect of velocity for different order frequencies - 5 m/s Initial speed")
-points(x=data.toPlot$TimeDiffValue[67], y=data.toPlot$Velocity[67], pch=1)
-points(x=data.toPlot$TimeDiffValue[117], y=data.toPlot$Velocity[117], pch=2)
-points(x=data.toPlot$TimeDiffValue[157], y=data.toPlot$Velocity[157], pch=3)
-points(x=data.toPlot$TimeDiffValue[187], y=data.toPlot$Velocity[187], pch=4)
-points(x=data.toPlot$TimeDiffValue[207], y=data.toPlot$Velocity[207], pch=5) 
-points(x=data.toPlot$TimeDiffValue[217], y=data.toPlot$Velocity[217], pch=6)
-points(x=data.toPlot$TimeDiffValue[225], y=data.toPlot$Velocity[225], pch=7) 
-points(x=data.toPlot$TimeDiffValue[231], y=data.toPlot$Velocity[231], pch=8) 
-points(x=data.toPlot$TimeDiffValue[234], y=data.toPlot$Velocity[234], pch=9) 
+     main="Effect of velocity for 1 second interval - 5 m/s Initial speed")
+#points(x=data.toPlot$TimeDiffValue[67], y=data.toPlot$Velocity[67], pch=1)
+#points(x=data.toPlot$TimeDiffValue[117], y=data.toPlot$Velocity[117], pch=2)
+#points(x=data.toPlot$TimeDiffValue[157], y=data.toPlot$Velocity[157], pch=3)
+#points(x=data.toPlot$TimeDiffValue[187], y=data.toPlot$Velocity[187], pch=4)
+#points(x=data.toPlot$TimeDiffValue[207], y=data.toPlot$Velocity[207], pch=5) 
+#points(x=data.toPlot$TimeDiffValue[217], y=data.toPlot$Velocity[217], pch=6)
+#points(x=data.toPlot$TimeDiffValue[225], y=data.toPlot$Velocity[225], pch=7) 
+#points(x=data.toPlot$TimeDiffValue[231], y=data.toPlot$Velocity[231], pch=8) 
+#points(x=data.toPlot$TimeDiffValue[234], y=data.toPlot$Velocity[234], pch=9) 
 
-legend("topright",  inset=c(-0.25,0), legend=c("5s interval", "4s interval", "3s interval", "2s interval", 
-                           "1s interval", "0.75s interval", "0.5s interval", "0.25s interval", 
-                           "0.1s interval"), bty="n", pch=1:9, lty=1, lwd=1)
+#legend("topright",  inset=c(-0.25,0), legend=c("5s interval", "4s interval", "3s interval", "2s interval", 
+#                           "1s interval", "0.75s interval", "0.5s interval", "0.25s interval", 
+#                           "0.1s interval"), bty="n", pch=1:9, lty=1, lwd=1)
 
-#dev.off()
+dev.off()
